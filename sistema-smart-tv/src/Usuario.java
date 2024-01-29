2d72ffa1c1c7b9a640cd78c9a7aab4e13e92bc43
Joelson Moises <95639983+JoelsonMoises@users.noreply.github.com>
2024-01-29 05:46:00 -0300

    projeto smart tv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,33 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv ligada ? " + smartTv.ligada);
+        System.out.println(" Canal atual : " + smartTv.canal);
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> Tv ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Tv ligada ? " + smartTv.ligada);
+
+        smartTv.diminuindoVolume();
+        smartTv.diminuindoVolume(); 
+        smartTv.diminuindoVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual : " + smartTv.volume);
+
+
+        smartTv.mudarCanal(13);
+        System.out.println(" Canal atual : " + smartTv.canal);
+
+        smartTv.dimunuirCanal();
+        System.out.println(" Canal atual : " + smartTv.canal);
+        
+
+    }
+
+        
+}
